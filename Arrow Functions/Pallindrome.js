f47c50f525e4b85650629a1038afb9f258c8715f
dhinadk28 <103361324+dhinadk28@users.noreply.github.com>
2022-05-05 18:44:06 +0530

    Create Pallindrome.js

---
+++ Arrow Functions/Pallindrome.js
@@ -0,0 +1,13 @@
+let arrayPallindrome = [111, 115, 989, 345, 616, 776]
+
+console.log("Pallindrome Numbers in array are: " +
+    arrayPallindrome.filter((item) => {
+
+        let temp = item + "";
+
+        if (temp.split('').reverse().join('') === item + "")
+            return true
+
+        return false
+
+    }))
