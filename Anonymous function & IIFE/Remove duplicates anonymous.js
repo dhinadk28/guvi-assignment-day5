dfd8b6025768d618b8e3981a623a2130908d3fde
dhinadk28 <103361324+dhinadk28@users.noreply.github.com>
2022-05-05 18:22:30 +0530

    Create Remove duplicates anonymous.js

---
+++ Anonymous function & IIFE/Remove duplicates anonymous.js
@@ -0,0 +1,30 @@
+var arr=function(a){
+    var b=[];
+    var n=a.length;
+    for(let i=0;i<n-1;i++)
+    {
+        for(let j=i+1;j<n;j++)
+        {
+            if(a[i]===a[j])
+            {
+                delete a[j]
+            }
+        }
+    }
+    for(let i=0;i<a.length;i++)
+    {
+        if(a[i]%10===Math.floor(a[i]%10))
+        {
+            b.push(a[i])
+        }
+    }
+    let c=""
+    for(let i=0;i<b.length-1;i++)
+     {
+         c+=b[i]+" ";
+    }
+    c+=b[b.length-1];
+     return c;
+    
+}
+console.log(arr([1,1,2,1,2,3,7,7,,3,4,3,39,9,3,2,8,8,2]));
