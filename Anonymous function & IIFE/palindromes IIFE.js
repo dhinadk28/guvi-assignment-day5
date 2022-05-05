a37e8c0370a6dddb3283cb3c5e677d9242c3ecfe
dhinadk28 <103361324+dhinadk28@users.noreply.github.com>
2022-05-05 18:11:17 +0530

    Create palindromes IIFE.js

---
+++ Anonymous function & IIFE/palindromes IIFE.js
@@ -0,0 +1,26 @@
+(function(a){
+    var res="";
+    var ans="";
+    for(i=0;i<a.length;i++)
+    {
+        res+=a[i];
+        var count=0
+        
+        for(j=0;j<res.length;j++)
+        {
+            if(res[j]===res[((res.length-1)-j)])
+            {
+                count++
+                
+            }
+        }
+        if(count===res.length)
+        {
+            ans+=res+" "
+        }
+        res="";
+    }
+    console.log(ans);
+ 
+}) ([1,44,565,411,576,626,1000,4884,999,499,505,303]);
+
